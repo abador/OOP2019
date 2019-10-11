8cb6dcee834f552e937740a05b40942e392a0a5b
braddock51 <47740798+braddock51@users.noreply.github.com>
2019-10-11 15:12:21 +0200

    Add files via upload

---
+++ Lesson1/150845/zad1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = new int[5] { 1, 2, 3, 4, 5 };
+
+            for (int i = 0; i < tab.Length; i++)
+                Console.Write($"{tab[i]},");
+            Console.Write("\n");
+            for (int i = 0; i < tab.Length; i++)
+                Console.WriteLine(tab[i]);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson1/150845/zad5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            while (true)
+            {
+                Console.WriteLine("Podaj liczbe");
+                n = int.Parse(Console.ReadLine());
+                if (n >= 5 && n <= 15)
+                    break;
+            }
+            
+            while (n>=0)
+            {
+
+                Console.WriteLine(n);
+                n--;
+            }
+            Console.ReadLine();
+        }
+    }
+}
