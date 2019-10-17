5c2cbe15802f8c0119a51192cd6feb826e6f6b8b
fifi7596 <fb7@vp.pl>
2019-10-17 20:46:46 +0200

    zad6 & 7

---
+++ 135570/zadanie6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace zadanie6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Zadanie 6
+
+            Console.WriteLine("Podaj liczbe od 5 do 15");
+            int wartosc = int.Parse(Console.ReadLine());
+            if (wartosc > 15 || wartosc < 5)
+            {
+                Console.WriteLine("Podales zla liczbe. Program zostanie zakonczony.");
+                System.Environment.Exit(0);
+            }
+            while(wartosc > -1)
+            {
+                Console.WriteLine("Wartosc wynosi {0}", wartosc);
+                wartosc--;
+            }
+        }
+    }
+}
---
+++ 135570/zadanie7.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace zadanie7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba;
+            do
+            {
+                Console.WriteLine("Podaj liczbę większą niż 5.");
+                liczba = int.Parse(Console.ReadLine());
+
+                if (liczba > 5)
+                {
+                    Console.WriteLine("Dobra liczba. Koniec programu.");
+                    System.Environment.Exit(0);
+                }
+
+                Console.WriteLine("Zła liczba.");
+
+            } while (liczba <= 5);
+        }
+    }
+}
