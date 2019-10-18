aaee0c1488e8617ede2fdbdfc76f37753ac7c91e
fifi7596 <fb7@vp.pl>
2019-10-18 16:15:17 +0200

    Add files via upload

---
+++ Lesson2/Car.cs
@@ -0,0 +1,37 @@
+﻿namespace zadania
+{
+    public class Car
+    {
+        private string marka;
+        private int rok;
+        private int przebieg;
+
+        public int jedz(int trasa)
+        {
+            this.przebieg += trasa;
+            return this.przebieg;
+        }
+
+        public string pobierzMarke()
+        {
+            return marka;
+        }
+        public int pobierzRok()
+        {
+            return rok;
+        }
+        public Car (string marka, int rok)
+        {
+            this.marka = marka;
+            this.rok = rok;
+        }
+
+        private string model;
+        private int iloscDrzwi;
+        private float pojemnoscSilnika;
+        public double srednieSpalanie;
+
+
+    }
+
+}
---
+++ Lesson2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string carName = "Mój samochód";
+            Console.WriteLine("Nazwa samochodu: {0}", carName);
+
+            Car car1 = new Car("Renault", 1995);
+            Car car2 = new Car("Mitsubishi", 1990);
+
+            //car1 = car2;
+
+            Console.WriteLine("{0}, {1}", car1.pobierzMarke(), car1.pobierzRok());
+            Console.WriteLine("{0}, {1}", car2.pobierzMarke(), car2.pobierzRok());
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
