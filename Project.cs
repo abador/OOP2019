df25270ac911ac80d74ba3b40782adc8e33bcf5a
Kristofer <47741934+kristofer375@users.noreply.github.com>
2019-10-18 14:33:53 +0200

    151268-w01

---
+++ Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zadanie 4
+            float netto;
+            float podatek;
+            float brutto;
+            Console.Write("Podaj wartość netto: ");
+            netto = float.Parse(Console.ReadLine());
+            Console.Write("Podaj stosowaną stawkę podatkową (%): ");
+            podatek = 1 + (float.Parse(Console.ReadLine())/100);
+            brutto = netto * podatek;
+            Console.WriteLine("Wartość brutto wynosi: {0}", brutto);
+            Console.ReadKey();
+        }
+    }
+}
