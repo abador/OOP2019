c730dd04cab1248631db83958d6ece04408fc1ad
Bartosz599 <47973458+Bartosz599@users.noreply.github.com>
2019-10-18 14:32:27 +0200

    Add files via upload

---
+++ Lesson2/zad1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("podaj temperature w stopniach C\n");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.Write(i+"C\n");
+            double z = (i * 1.8) + 32;
+            Console.Write(z + "F");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
