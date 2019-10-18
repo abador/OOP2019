9e0d26579f9077ead639d5b1cd30f099f7dead84
Kristofer <47741934+kristofer375@users.noreply.github.com>
2019-10-18 16:17:17 +0200

    151268 lesson2
    Zadanie 1-5 zrobione

---
+++ Lesson2/Lesson2/Project.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string carName = "Mój samochód";
+            Console.WriteLine(carName);
+            Car car1 = new Car("Subaru", 2000, 7.3);
+            Car car2 = new Car("Kia", 2014, 5.6);
+            car1.Pokaz();
+            car2.Pokaz();
+            //Przepisanie car2 do car1
+            car2.Jedz(100);
+            Console.WriteLine();
+            car1 = car2;
+            car1.Pokaz();
+            car2.Jedz(100);
+            car2.Pokaz();
+            //Console.WriteLine("{0}"car2.ObliczSpalanie(100));
+            Console.ReadKey();
+        }
+    }
+}
