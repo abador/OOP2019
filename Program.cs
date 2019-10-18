8fb743a02a8340f34f4985d708008ea23b292760
mtrzcinska <47740869+mtrzcinska@users.noreply.github.com>
2019-10-18 14:46:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1: ");
+            string haslo1 = Console.ReadLine();
+            Console.WriteLine("2: ");
+            string haslo2 = Console.ReadLine();
+
+            if (haslo1 == haslo2)
+                Console.WriteLine("takie same");
+            else
+                Console.WriteLine("inne");
+
+            Console.ReadKey();
+        }
+    }
+}
