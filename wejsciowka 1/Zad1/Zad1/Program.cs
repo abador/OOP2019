79e77064be44322f287015c32e8fea4863e3ed49
zw1nc3k <zwincek@hotmail.com>
2019-10-18 14:31:55 +0200

    Add files via upload

---
+++ wejsciowka 1/Zad1/Zad1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj temperature Celcjusza");
+            double a = 0;
+            a = double.Parse(Console.ReadLine());
+            double temperatura = 0;
+            temperatura = (a * 1.8) + 32;
+            Console.WriteLine("Podałeś " + a + " Stopni Celcjusza");
+            Console.WriteLine("Podana temperatura równa się " + temperatura + " stopni Fahrenheita");
+            Console.ReadKey();
+        }
+    }
+}
