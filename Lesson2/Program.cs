529acd1f97eebaaad4a2b8810309290cbd9058b4
Rendianor <47740877+Rendianor@users.noreply.github.com>
2019-10-18 14:37:33 +0200

    Add files via upload

---
+++ Lesson2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//2
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mila;
+            Console.WriteLine("Podaj mile: ");
+            mila = Convert.ToInt32(Console.ReadLine());
+            int wynik;
+            wynik = (mila * 1,609344);
+            Console.WriteLine("wynik wynosi ");
+            wynik = Convert.ToInt32(Console.ReadLine());
+            Console.ReadKey();
+        }
+    }
+}
