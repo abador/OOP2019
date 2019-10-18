888480760d12dbe22af9c345c0429b054339d57c
braddock51 <47740798+braddock51@users.noreply.github.com>
2019-10-18 14:29:37 +0200

    Add files via upload

---
+++ Wejsciowka01 150845/Wejsciowka01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wejsciowka01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int masa = 85;
+            double wzrost = 1.9;
+
+            Console.WriteLine($"Masa = {masa}\nWzrost = {wzrost}\nBMI  = {bmi(masa, wzrost)}");
+
+            Console.ReadLine();
+        }
+
+        static double bmi(int masa, double wzrost)
+        {
+            double bmi = masa / (wzrost * wzrost);
+
+            return bmi;
+        }
+    }
+}
