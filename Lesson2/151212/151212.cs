d78fe0bbff44648425577a3d89f065bec23bde91
Ryzerr98 <47741010+Ryzerr98@users.noreply.github.com>
2019-10-18 14:52:10 +0200

    Add files via upload

---
+++ Lesson2/151212/151212.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            float zmienna2;
+            string zmienna = NewMethod();
+            zmienna2 = float.Parse(zmienna);
+            float zmienna3;
+            zmienna = NewMethod2();
+            zmienna3 = float.Parse(zmienna);
+            float zmienna4 = zmienna2 * zmienna3 + zmienna2;
+            Console.WriteLine("Wartośc brutto to");
+            Console.Write(zmienna4);
+            Console.ReadKey();
+        }
+
+        private static string NewMethod()
+        {
+            Console.WriteLine("Podaj wartość netto");
+            return Console.ReadLine();
+        }
+        private static string NewMethod2()
+        {
+            Console.WriteLine("Podaj stawkę podatkową");
+            return Console.ReadLine();
+        }
+    }
+}
