85d3c11393c969729c8e878e6d6f4fb4da1af7b1
braddock51 <47740798+braddock51@users.noreply.github.com>
2019-10-25 14:27:02 +0200

    Add files via upload

---
+++ Lesson3/Wejsciowka02-150845/Wejsciowka02-150845/Czworokat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wejsciowka02_150845
+{
+    class Czworokat
+    {
+        int pierwszyBok;
+        int drugiBok;
+        int trzeciBok;
+        int czwartBok;
+
+        public Czworokat(int pierwszyBok, int drugiBok, int trzeciBok, int czwartyBok)
+        {
+            this.pierwszyBok = pierwszyBok;
+            this.drugiBok = drugiBok;
+            this.trzeciBok = trzeciBok;
+            this.czwartBok = czwartyBok;
+        }
+        public void CzyRomb()
+        {
+            if (pierwszyBok < 0 || drugiBok < 0 || trzeciBok < 0 || czwartBok < 0)
+                Console.WriteLine("Nie jest rombem");
+            else if (pierwszyBok != drugiBok || pierwszyBok != trzeciBok || pierwszyBok != czwartBok || drugiBok != trzeciBok || drugiBok != czwartBok || trzeciBok != czwartBok)
+                Console.WriteLine("Niej jest rombem");
+            else
+                Console.WriteLine("Jest rombem");
+        }
+
+    }
+}
---
+++ Lesson3/Wejsciowka02-150845/Wejsciowka02-150845/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wejsciowka02_150845
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Czworokat x = new Czworokat(4, 4, 4, 4);
+
+            x.CzyRomb();
+
+            Console.ReadLine();
+
+        }
+    }
+}
