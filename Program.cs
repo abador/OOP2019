1194748065e820cd12ee3d323c756b28003a88fe
Ryzerr98 <47741010+Ryzerr98@users.noreply.github.com>
2019-10-25 14:32:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _151212w2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Czworokat pierwszy = new Czworokat();
+            pierwszy.CzyRomb(1, 1, 1, 1);
+            Console.ReadKey();
+        }
+    }
+}
