bca2ef1fb0c9a6ae8fef182884609d8d8ca272ba
Rendianor <47740877+Rendianor@users.noreply.github.com>
2019-10-25 14:30:10 +0200

    Add files via upload

---
+++ Lesson3/Lesson3/ConsoleApp7/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Szkola
+    {
+        public int liczbaUczniow;
+        public int liczbaLawek;
+        public string komunikat;
+        public void SprawdzLawki()
+        {
+            if (liczbaUczniow < liczbaLawek)
+            {
+                komunikat = "sa lawki";
+            }
+            else
+                komunikat = "nie ma wolnej lawki";
+        }
+    }
+}
---
+++ Lesson3/Lesson3/ConsoleApp7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Szkola uczniowie = new Szkola();
+            uczniowie.liczbaLawek = 20;
+            uczniowie.liczbaUczniow = 10;
+            
+            uczniowie.SprawdzLawki();
+            Console.WriteLine(uczniowie.komunikat);
+            Console.WriteLine(uczniowie.liczbaLawek);
+            Console.ReadKey();
+        }
+    }
+}
