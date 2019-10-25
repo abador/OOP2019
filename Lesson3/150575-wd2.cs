c5043b6d94eae77d5d0e71cba7b2cd6b15b9e6ae
Bartosz599 <47973458+Bartosz599@users.noreply.github.com>
2019-10-25 14:29:01 +0200

    Add files via upload

---
+++ Lesson3/150575-wd2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+ 
+    int PodajCeneZaMetr(int liczbaLudnosci)
+    {
+        int cena = liczbaLudnosci * 0.01;
+        return cena;
+    }
+
+    class Miasto
+    {
+        public int liczbaLudnosci;
+        public string NazwaMiasta;
+        PodajCeneZaMetr(public int liczbaLudnosci);
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
