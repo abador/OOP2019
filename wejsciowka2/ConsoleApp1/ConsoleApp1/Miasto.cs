d3ae8b8fa82c7f4a5db3cbb82885a8da52cd9047
zw1nc3k <zwincek@hotmail.com>
2019-10-25 14:28:15 +0200

    Add files via upload

---
+++ wejsciowka2/ConsoleApp1/ConsoleApp1/Miasto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Miasto
+    {
+        public int liczbaLudnosci;
+        public string nazwaMiasta;
+        public Miasto(int liczbaLudnosci, string nazwaMiasta)
+        {
+            this.liczbaLudnosci = liczbaLudnosci;
+            this.nazwaMiasta = nazwaMiasta;
+        }
+        public int PodajCeneZaMetr()
+        {
+            if (liczbaLudnosci > 10000)
+            {
+                return 150;
+            }
+            else if (liczbaLudnosci < 100000 & liczbaLudnosci > 25000)
+            {
+                return 80;
+            }
+            else return 40;
+        }
+    }
+}
---
+++ wejsciowka2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Miasto Warszawa = new Miasto(500000, "Warszawa");
+            Miasto Ciechanow = new Miasto(18000, "Ciechanow");
+            Miasto Wroclaw = new Miasto(60000, "Wroclaw");
+            Console.WriteLine(Warszawa.nazwaMiasta + " Cena za metr " + Warszawa.PodajCeneZaMetr());
+            Console.ReadKey();
+        }
+    }
+}
