6beefcfc4a99b5615064b0255a21da22d37df45a
braddock51 <47740798+braddock51@users.noreply.github.com>
2019-11-15 13:02:15 +0100

    Update Mapa.cs

--- zadanie-zamiast-wejsciowki/zadanie-zamiast-wejsciowki/Mapa.cs
+++ zadanie-zamiast-wejsciowki/zadanie-zamiast-wejsciowki/Mapa.cs
@@ -3,19 +3,41 @@
 
 namespace zadanie_zamiast_wejsciowki
 {
-    class Mapa : IMapa
+    class Mapa : Point, IMapa
     {
-        private int[] tab;
-
-        public Mapa(int lenght)
-        {
-            this.tab = new int[lenght];
-        }
+        private MapElement[,] tab;
 
         
-        public void Generate()
+        public void Generate(int lenghtX, int lenghtY)
         {
-            throw new NotImplementedException();
+            this.x = lenghtX;
+            this.y = lenghtY;
+            
+            this.tab = new MapElement[this.x, this.y];
+
+            for(int i=0; i<this.x; i++)
+            {
+                for(int j=0; j<this.y; j++)
+                {
+                    Random rnd = new Random();
+                    int los = rnd.Next(1, 4);
+
+                    switch (los)
+                    {
+                        case 1:
+                            tab[i, j] = new Grass();
+                            break;
+                        case 2:
+                            tab[i, j] = new Tree();
+                            break;
+                        case 3:
+                            tab[i, j] = new Rock();
+                            break;
+
+                    }
+
+                }
+            }
         }
 
         public void List()
