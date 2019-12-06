5bc9255d2bb0fd0ec2317f71a2ae93b9bca6d9ac
Kristofer <47741934+kristofer375@users.noreply.github.com>
2019-12-06 11:30:39 +0100

    Zadanie z dnia 30 listopada
    Nie wykonanie wszystkich punktów.
    Powinny działać do punktu 7
    Pozostałe nie zrozumiałem w wykonaniu.

---
+++ Lesson7/151268-z07-Biblioteka/Osoba.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteka
+{
+    public class Osoba
+    {
+        protected string imie;
+        protected string nazwisko;
+        public Osoba()
+        {
+            imie = "brak";
+            nazwisko = "brak";
+        }
+        public Osoba(string imie, string nazwisko)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+        }
+    }
+    public class Autor : Osoba
+    {
+        private string narodowosc;
+        public Autor()
+        {
+            narodowosc = "brak";
+        }
+        public Autor(string narodowosc, string imie, string nazwisko) : base(imie, nazwisko)
+        {
+            this.narodowosc = narodowosc;
+        }
+    }
+    abstract public class Pozycja
+    {
+        protected string tytul;
+        protected int id;
+        protected string wydawnictwo;
+        protected int rokWydania;
+        public Pozycja()
+        {
+            tytul = "brak";
+            id = 0;
+            wydawnictwo = "brak";
+            rokWydania = 0;
+        }
+        public Pozycja(string tytul, int id, string wydawnictwo, int rokWydania)
+        {
+            this.tytul = tytul;
+            this.id = id;
+            this.wydawnictwo = wydawnictwo;
+            this.rokWydania = rokWydania;
+        }
+        abstract public void WypiszInfo();
+    }
+    public class Ksiazka : Pozycja
+    {
+        private int liczbaStron;
+        //pole referencyjne Autor autor
+        public Autor autor;
+        public Ksiazka()
+        {
+            liczbaStron = 0;
+        }
+        public Ksiazka(Autor autor, int liczbaStron, string tytul, int id, string wydawnictwo, int rokWydania) : base(tytul, id, wydawnictwo, rokWydania)
+        {
+            this.liczbaStron = liczbaStron;
+        }
+        public override void WypiszInfo()
+        {
+            Console.WriteLine($"Tytuł - {tytul}");
+            Console.WriteLine($"ID - {id}");
+            Console.WriteLine($"Wydawnictwo - {wydawnictwo}");
+            Console.WriteLine($"Rok Wydania - {rokWydania}");
+            Console.WriteLine($"Liczba stron - {liczbaStron}");
+            Console.WriteLine($"Autor -  {autor}"); //??
+            //throw new NotImplementedException();
+        }
+    }
+    public class Czasopismo : Pozycja
+    {
+        private int numer;
+        public Czasopismo()
+        {
+            numer = 0;
+        }
+        public Czasopismo(int numer, string tytul, int id, string wydawnictwo, int rokWydania) : base(tytul, id, wydawnictwo, rokWydania)
+        {
+            this.numer = numer;
+        }
+        public override void WypiszInfo()
+        {
+            Console.WriteLine($"Tytuł - {tytul}");
+            Console.WriteLine($"ID - {id}");
+            Console.WriteLine($"Wydawnictwo - {wydawnictwo}");
+            Console.WriteLine($"Rok Wydania - {rokWydania}");
+            Console.WriteLine($"Numer - {numer}");
+            //throw new NotImplementedException();
+        }
+    }
+    public class Katalog
+    {
+        private string dzialTematyczny;
+        //pole referencyjne listaPozycji typu List<Pozycja>
+        //public List<Pozycja> listaPozycji;
+        public Katalog()
+        {
+            dzialTematyczny = "brak";
+        }
+        public Katalog(string dzialTematyczny)
+        {
+            this.dzialTematyczny = dzialTematyczny;
+        }
+        //DodajPozycje(pozycja)
+        //public void DodajPozycje(Pozycja)
+        //{
+        //
+        //}
+    }
+    //interface IZarzadzanieKatalogiem
+    //{
+    //
+    //}
+}
---
+++ Lesson7/151268-z07-Biblioteka/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Biblioteka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba osoba = new Osoba("yes", "no");
+            Autor autor = new Autor("xD", "yes", "no");
+            Ksiazka ksiazka = new Ksiazka();
+            ksiazka.WypiszInfo();
+            Czasopismo czasopismo = new Czasopismo();
+            czasopismo.WypiszInfo();
+        }
+    }
+}
